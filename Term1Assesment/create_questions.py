d354e7e74cb4aed237c67dbf39c25a19696d4dac
Josh <68841480+JoshMono@users.noreply.github.com>
2024-04-10 11:45:43 +1000

    Hide and Show Password Btn

--- Term1Assesment/create_questions.py
+++ Term1Assesment/create_questions.py
@@ -1,2 +1,63 @@
 def Question1():
+    question = ""
+    difficulty = 1
+
+def Question2():
     pass 
+
+def Question3():
+    pass 
+
+def Question4():
+    pass 
+
+def Question5():
+    pass 
+
+def Question6():
+    pass 
+
+def Question7():
+    pass 
+
+def Question8():
+    pass 
+
+def Question9():
+    pass 
+
+def Question10():
+    pass 
+
+def Question11():
+    pass 
+
+def Question12():
+    pass 
+
+def Question13():
+    pass 
+
+def Question14():
+    pass 
+
+def Question15():
+    pass 
+
+def Question16():
+    pass 
+
+def Question17():
+    pass 
+
+def Question18():
+    pass 
+
+def Question19():
+    pass 
+
+def Question20():
+    pass 
+
+
+
--- Term1Assesment/login_create.py
+++ Term1Assesment/login_create.py
@@ -1,41 +1,45 @@
 from tkinter import *
-from PIL import ImageTk
+from PIL import ImageTk, Image
+
 
 
 
 def login(login_btn, create_btn, title_lbl, root):
-    hide = True
     
-    def hide_show(hide):
-        print(hide)
-        if hide:
-            hide_show_btn.config(image=show_image)
-            password_input.config(show="")
-            hide = False
-        else:
-            hide = True
-
+    
+    
     login_btn.destroy()
     create_btn.destroy()
     title_lbl.destroy()
+
+    hide213 = True
+    
+    def show():
+        hide_show_btn.config(command=lambda:hide(), text="Hide")   
+        password_input.config(show="")
+
+    def hide():
+        hide_show_btn.config(command=lambda:show(), text="Show")   
+        password_input.config(show="*")
+        
+        
+    
     
     title_lbl = Label(root, text="Login", font=20)
     title_lbl.place(relx=0.5, rely=0.13, anchor='center')
     
-    nesa_lbl = Label(root, text="Nesa")
+    nesa_lbl = Label(root, text="Nesa") 
     password_lbl = Label(root, text="Password")
-    
-    hide_image = ImageTk.PhotoImage(file='images//eye-hide.png')
-    show_image = ImageTk.PhotoImage(file='images//eye-show.png')
     
     nesa_input = Entry(root)
     password_input = Entry(root, show="*")
-    hide_show_btn = Button(root, background='white', command=lambda: hide_show(hide))
+    hide_show_btn = Button(root, background='white', command=lambda: show(), text="Show" ,bg='white')
 
-    print(hide_show_btn.image_names)
     nesa_input.place(relx=0.5, rely=0.5, relwidth=0.31, relheight=0.08, anchor='center')
     password_input.place(relx=0.5, rely=0.6, relwidth=0.31, relheight=0.08, anchor='center')
-    hide_show_btn.place(relx=0.7, rely=0.6, relwidth=0.05, relheight=0.08, anchor='center')
+    hide_show_btn.place(relx=0.7, rely=0.6, relwidth=0.075, relheight=0.08, anchor='center')
+
+    submit_btn = Button(root, text="Login")
     
     nesa_lbl.place(relx=0.3, rely=0.5, relwidth=0.11, relheight=0.08, anchor='e')
     password_lbl.place(relx=0.3, rely=0.6, relwidth=0.11, relheight=0.08, anchor='e')
