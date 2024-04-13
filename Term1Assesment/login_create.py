38077f1fa28c515662d0cf641fc57b497d3e38df
trevmono <trevmono@hotmail.com>
2024-04-13 16:14:18 +1000

    add questions as data

--- Term1Assesment/login_create.py
+++ Term1Assesment/login_create.py
@@ -1,13 +1,7 @@
-from tkinter import *
-from PIL import ImageTk, Image
+import tkinter as tk
 from create_questions import main
 
-
-
-
 def login(login_btn, create_btn, title_lbl, root):
-    
-    
     
     login_btn.destroy()
     create_btn.destroy()
@@ -26,21 +20,21 @@ def login(login_btn, create_btn, title_lbl, root):
         
     
     
-    title_lbl = Label(root, text="Login", font=20)
+    title_lbl = tk.Label(root, text="Login", font=20)
     title_lbl.place(relx=0.5, rely=0.13, anchor='center')
     
-    nesa_lbl = Label(root, text="Nesa") 
-    password_lbl = Label(root, text="Password")
+    nesa_lbl = tk.Label(root, text="Nesa") 
+    password_lbl = tk.Label(root, text="Password")
     
-    nesa_input = Entry(root)
-    password_input = Entry(root, show="*")
-    hide_show_btn = Button(root, background='white', command=lambda: show(), text="Show" ,bg='white')
+    nesa_input = tk.Entry(root)
+    password_input = tk.Entry(root, show="*")
+    hide_show_btn = tk.Button(root, background='white', command=lambda: show(), text="Show" ,bg='white')
 
     nesa_input.place(relx=0.5, rely=0.5, relwidth=0.31, relheight=0.08, anchor='center')
     password_input.place(relx=0.5, rely=0.6, relwidth=0.31, relheight=0.08, anchor='center')
     hide_show_btn.place(relx=0.7, rely=0.6, relwidth=0.075, relheight=0.08, anchor='center')
 
-    submit_btn = Button(root, text="Login", command=lambda: main(root, title_lbl, nesa_lbl, password_lbl, nesa_input, password_input, submit_btn, hide_show_btn))
+    submit_btn = tk.Button(root, text="Login", command=lambda: main(root, title_lbl, nesa_lbl, password_lbl, nesa_input, password_input, submit_btn, hide_show_btn))
     submit_btn.place(relx=0.3, rely=0.8, relwidth=0.11, relheight=0.08, anchor='e')
     
     nesa_lbl.place(relx=0.3, rely=0.5, relwidth=0.11, relheight=0.08, anchor='e')
---
+++ Term1Assesment/questions_data.py
@@ -0,0 +1,45 @@
+QUESTIONS = [
+    {"question_id": 1,
+     "question_text": "How many days in a week?",
+     "difficulty": 2,
+     "type_of_question": "multi",
+     "answer": "7",
+     "choices": [
+         "1",
+         "3",
+         "7",
+         "10"
+     ]
+     },
+     {"question_id": 2,
+     "question_text": "How many months in a year?",
+     "difficulty": 1,
+     "type_of_question": "multi",
+     "answer": "12",
+     "choices": [
+         "1",
+         "3",
+         "7",
+         "12"
+     ]
+     },
+     {"question_id": 3,
+     "question_text": "How many years in a century?",
+     "difficulty": 3,
+     "type_of_question": "multi",
+     "answer": "100",
+     "choices": [
+         "10",
+         "30",
+         "100",
+         "1000"
+     ]
+     },
+    #  {"question_id": 4,
+    #  "question_text": "What is the day after Sunday",
+    #  "difficulty": 3,
+    #  "type_of_question": "fill",
+    #  "answer": "Monday"
+    #  },
+
+]
