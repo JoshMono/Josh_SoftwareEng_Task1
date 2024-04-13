4a687ba70a93d3fbe28f0bea1507423e5df63030
JoshMono <joshmono2008@outlook.com>
2024-04-13 16:42:00 +1000

    add questions as data

--- Term1Assesment/create_questions.py
+++ Term1Assesment/create_questions.py
@@ -1,18 +1,21 @@
-from tkinter import *
+import tkinter as tk
 import random as rand
 
 
-
 class Question:
-    def __init__(self, question, difficulty, type_of_question, root, answer, ans1=None, ans2=None, ans3=None):
-        self.question = question
+    def __init__(self, question_text, difficulty, type_of_question, root, answer, choices=None):
+        self.question_text = question_text
         self.difficulty = difficulty
         self.type_of_question = type_of_question
         self.root = root
         self.answer = answer
-        self.ans1 = ans1
-        self.ans2 = ans2
-        self.ans3 = ans3
+        self.choices = choices
+        # self.ans1 = ans1
+        # self.ans2 = ans2
+        # self.ans3 = ans3
+    
+    def __str__(self) -> str:
+        return F"Question Text: {self.question_text}"
     
     def get_question_type(self):
         if self.type_of_question == 'multi':
@@ -26,13 +29,21 @@ class Question:
 
     def create_page(self):
         self.get_question_type()
-        
-
 
 def main(root, a, b, c, d, e, f, g):
-    all_questions = [Question("123123213", 2, 'multi', root, "Right", "Wrong1", "Wrong2", "Wrong3")]
-    print(all_questions)
-    
+    all_questions = []
+    from questions_data import QUESTIONS
+    for question in QUESTIONS:
+        q = Question(
+            question_text=question.get('question_text'),
+            difficulty=question.get('difficulty'),
+            type_of_question=question.get('type_of_question'),
+            root=root,
+            answer=question.get('answer'),
+            choices=question.get('choices')
+            )
+        all_questions.append(q)
+
     a.destroy()
     b.destroy()
     c.destroy()
@@ -44,35 +55,32 @@ def main(root, a, b, c, d, e, f, g):
     give_question(all_questions)
 
 
-def multiple_choice(Self):
+def multiple_choice(question):
     
-    
-    print(Self.question)
-    question_lbl = Label(Self.root, text=Self.question)
+    question_lbl = tk.Label(question.root, text=question.question_text)
     question_lbl.place(relx=0.5, rely=0.3, anchor='center')
     
-    group = IntVar()
+    group = tk.IntVar()
     
-    answer_1 = Radiobutton(Self.root, text="Option 1", variable=group, value=1)
+    answer_1 = tk.Radiobutton(question.root, text="Option 1", variable=group, value=1)
     answer_1.place(relx=0.2, rely=0.6, anchor='center')
     
-    answer_2 = Radiobutton(Self.root, text="Option 2", variable=group, value=2)
+    answer_2 = tk.Radiobutton(question.root, text="Option 2", variable=group, value=2)
     answer_2.place(relx=0.4, rely=0.6, anchor='center')
     
-    answer_3 = Radiobutton(Self.root, text="Option 3", variable=group, value=3)
+    answer_3 = tk.Radiobutton(question.root, text="Option 3", variable=group, value=3)
     answer_3.place(relx=0.6, rely=0.6, anchor='center')
     
-    answer_4 = Radiobutton(Self.root, text="Option 4", variable=group, value=4)
+    answer_4 = tk.Radiobutton(question.root, text="Option 4", variable=group, value=4)
     answer_4.place(relx=0.8, rely=0.6, anchor='center')
     
     radio_list = [answer_1, answer_2, answer_3, answer_4]
-    answer_list = [Self.answer, Self.ans1, Self.ans2, Self.ans3]
 
-    for i in range(4):
+    for _ in range(4):
         random1_num = rand.randint(0, len(radio_list) - 1)
-        random2_num = rand.randint(0, len(answer_list) - 1)
+        random2_num = rand.randint(0, len(question.choices) - 1)
         radio_btn = radio_list.pop(random1_num)
-        temp_answer = answer_list.pop(random2_num)
+        temp_answer = question.choices.pop(random2_num)
         radio_btn.config(text=f"{temp_answer}")
         
         
@@ -88,8 +96,6 @@ def give_question(all_questions):
     random_num = rand.randint(0, len(all_questions) - 1)
     question = all_questions.pop(random_num)
     question.create_page()
-
-    print(all_questions)
 
 
 
--- Term1Assesment/main.py
+++ Term1Assesment/main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 from tkinter import font
 import login_create
 from login_create import login
@@ -6,18 +6,16 @@ from login_create import login
 def test():
     pass
 
-root = Tk()
+root = tk.Tk()
 root.geometry('450x300')
 
-title_lbl = Label(root, text="Software Engineering Test", font=20)
+title_lbl = tk.Label(root, text="Software Engineering Test", font=20)
 title_lbl.place(relx=0.5, rely=0.13, anchor='center')
 
-login_btn = Button(root, text="Login", command=lambda: login(login_btn, create_btn, title_lbl, root))
-create_btn = Button(root, text="Create", command=lambda: create_account())
+login_btn = tk.Button(root, text="Login", command=lambda: login(login_btn, create_btn, title_lbl, root))
+create_btn = tk.Button(root, text="Create", command=lambda: login_create.create_account())
 
 login_btn.place(relx=0.4, rely=0.5, relwidth=0.11, relheight=0.08, anchor='center')
 create_btn.place(relx=0.6, rely=0.5, relwidth=0.11, relheight=0.08, anchor='center')
-
-
 
 root.mainloop()
